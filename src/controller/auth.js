d206a849642d023657713e22901a29814e47cf67
Ragesh-A <ragesha2017@gmail.com>
2023-03-02 16:34:53 +0530

    bug fix

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -24,6 +24,7 @@ exports.signup = async (req, res) => {
     req.session.user = req.body;
     res.render('otp', { message: req.session.message });
   } catch (error) {
+    console.log(error);
     res.redirect('/account/signup');
   }
 };
