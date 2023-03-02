780c14924af48125389fe41d8e265a8b360a568d
Ragesh-A <ragesha2017@gmail.com>
2023-03-02 13:46:18 +0530

    bug fix

--- src/app.js
+++ src/app.js
@@ -8,15 +8,21 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 
-const indexRouter = require('./routes/index');
-mongoose.set('strictQuery', true);
-const app = express();
 env.config();
+let PORT = process.env.PORT || 3000;
+const DB = process.env.NEW_DATABASE_URL;
+
+//DATABASE CALL
+mongoose.set('strictQuery', true);
+connectDataBase()
+
+const indexRouter = require('./routes/index');
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.use(nocache());
 app.use(flash());
 app.use(cookieParser());
@@ -32,10 +38,24 @@ app.use(
   })
 );
 
-app.use('/', indexRouter);
-app.get('/t',(req, res) => {
-  res.send(req.query.page)
-})
-app.get('*', (req, res) => res.render('404'))
+app.use('/', indexRouter); 
+app.get('*', (req, res) => res.render('404'));
+
+//DTABASE CONNECTION CODE
+function connectDataBase(){
+  mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    // console.log("Atlas is connnected");
+    app.listen(PORT, () => {
+      console.log(`server running on port ${PORT}`)
+    });
+  })
+  .catch((err)=>{
+    console.log(err);
+    connectDataBase()
+  });
+}
+
 
 module.exports = app;
